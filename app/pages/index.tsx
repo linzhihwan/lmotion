c50ae944495e9f67bdf196d1535c4380a4824d95
linzhihwan <lmotion75@naver.com>
2025-12-19 10:54:44 +0900

    firtst commit

---
+++ app/pages/index.tsx
@@ -0,0 +1,10 @@
+import NavBar from "./NavBar";
+
+export default function Home() {
+  return (
+    <div>
+      <NavBar />
+      <h1>Welcome to Home Page</h1>
+    </div>
+  );
+}
--- app/test/page.tsx
+++ app/test/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-      <h1>Welcome to My App HOME</h1>
+      <h1>1111111111111111111</h1>
     </>
   );
 }
